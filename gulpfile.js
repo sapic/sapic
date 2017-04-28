ce8fd4a5a03cb2ba16792ea489a82b694ced662f
Tim <truecarry@gmail.com>
2017-04-28 20:48:42 +0300

    Revert gulp

--- gulpfile.js
+++ gulpfile.js
@@ -31,9 +31,7 @@ gulp.task('js', ['css2'], function(){
     fs.writeFileSync('./js/index.js', content.replace('{{#vernum}}', process.env.CIRCLE_BUILD_NUM));
     return gulp.src(['./js/jquery.min.js', './js/*'])
         .pipe(concat('main.js'))
-        .pipe(uglify({
-          mangle: true,
-        }))
+        .pipe(uglify())
         .pipe(gulp.dest('./out'));
 });
 gulp.task('html', ['js'], function(){
