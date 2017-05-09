e305a89654a812852f63b7be2b339c249356e6a8
The Oddball <oddball@oddball.tf>
2017-05-09 05:38:34 -0700

    another oops

--- js/index.js
+++ js/index.js
@@ -500,9 +500,11 @@ $(function() {
         $(this).removeClass("fa-spin");
    });
 
+   addArrows();
+
   loginFunc();
   setTimeout(function(){
-    if(!fuckAdBlock) {
+    if(typeof fuckAdBlock === 'undefined') {
         noAds();
     }
   }, 10);
@@ -515,7 +517,6 @@ $(function() {
     createInventory(userId);
   } else {
     $('#steamAuth').append('<a href="https://steamcommunity.com/openid/login?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=http%3A%2F%2Fsteam.design%2Findex.html%23login&openid.realm=http%3A%2F%2Fsteam.design&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select" class="name"><img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png" width="129" height="25"></a>');
-    addArrows();
   }
 
   reloadImages();
