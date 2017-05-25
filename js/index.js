89917ae7303776f9d23853c35d3045bd92fd29d0
Tim <truecarry@gmail.com>
2017-05-25 17:02:46 +0300

    Fix dummy if

--- js/index.js
+++ js/index.js
@@ -464,7 +464,7 @@ $(function() {
    }
 
    var bgs = store.get('bgs');
-   if (bgs && !bgs === undefined) {
+   if (!bgs) {
       var expire = new Date().getTime() + 86400000;
       $.ajax('https://steam.design/bg.json').done(function(data) {
          store.set('bgs', data, expire);
