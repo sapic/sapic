2c397ae5f7b0554bfa578eb02b9db13f484d749a
Tim <truecarry@gmail.com>
2020-10-29 18:22:36 +0300

    Remove 1xbet ads

--- src/components/pages/index/Ads.vue
+++ src/components/pages/index/Ads.vue
@@ -1,34 +1,18 @@
 <template>
   <div class="ads__container">
-    <Xbet v-if="isRu" />
-    <SteamLevels v-else />
+    <SteamLevels />
   </div>
 </template>
 
 <script>
 import SteamLevels from './ads/SteamLevels'
-import Xbet from './ads/Xbet'
 
 export default {
   components: {
     SteamLevels,
-    Xbet,
   },
 
   computed: {
-    isRu () {
-      if (!navigator || !navigator.languages) {
-        return false
-      }
-
-      for (const l of navigator.languages) {
-        if (l.indexOf('ru') !== -1) {
-          return true
-        }
-      }
-
-      return false
-    },
   },
 }
 </script>
