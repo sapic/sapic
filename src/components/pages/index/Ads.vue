afdfb1b5bec128459be52ae0f1cfa8aec6eed08d
Gwen <oddball@oddball.tf>
2024-05-19 12:34:07 -0700

    Switch SLU to DuoBot

--- src/components/pages/index/Ads.vue
+++ src/components/pages/index/Ads.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="infofriends__container">
-    <SteamLevelu />
+    <duobot />
   </div>
 </template>
 
 <script>
 // import SteamLevels from './ads/SteamLevels.vue'
 // import tradeit from './ads/tradeit.vue'
-import SteamLevelu from './ads/SteamLevelu.vue'
+//import SteamLevelu from './ads/SteamLevelu.vue'
+import duobot from './ads/DuoBot.vue'
 
 export default {
   components: {
-    SteamLevelu,
-    // tradeit,
+    duobot
   },
 
   computed: {},
