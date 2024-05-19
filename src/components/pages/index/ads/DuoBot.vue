cf647621b2b4c191b9a780dad1d0ccf79837720c
Gwen <oddball@oddball.tf>
2024-05-19 13:11:58 -0700

    Oops, fix link

--- src/components/pages/index/ads/DuoBot.vue
+++ src/components/pages/index/ads/DuoBot.vue
@@ -1,34 +1,38 @@
 <template>
-    <a class="profile_video" href="https://steamlevels.com" target="_blank" rel="noreferrer noopener">
-      <video ref="videoRef" autoplay muted playsinline loop />
-    </a>
-  </template>
-  
-  <script lang="ts" setup>
-  import SteamLevels from '@/assets/images/video-duobot.webm'
-  import { nextTick, ref } from 'vue'
-  
-  const videoRef = ref<HTMLVideoElement | null>(null)
-  setTimeout(() => {
-    nextTick(async () => {
-      if (videoRef.value) {
-        videoRef.value.src = SteamLevels
-      }
-    })
-  }, 128)
-  </script>
-  
-  <style lang="stylus" scoped>
-  .profile_video
+  <a
+    class="profile_video"
+    href="https://duobot.com/p/steamdesign"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <video ref="videoRef" autoplay muted playsinline loop />
+  </a>
+</template>
+
+<script lang="ts" setup>
+import SteamLevels from '@/assets/images/video-duobot.webm'
+import { nextTick, ref } from 'vue'
+
+const videoRef = ref<HTMLVideoElement | null>(null)
+setTimeout(() => {
+  nextTick(async () => {
+    if (videoRef.value) {
+      videoRef.value.src = SteamLevels
+    }
+  })
+}, 128)
+</script>
+
+<style lang="stylus" scoped>
+.profile_video
+  width 280px
+  height 560px
+  display block
+  margin 10px auto
+  position relative
+  overflow hidden
+  border-radius 10px
+
+  video
     width 280px
-    height 560px
-    display block
-    margin 10px auto
-    position relative
-    overflow hidden
-    border-radius 10px
-  
-    video
-      width 280px
-  </style>
-  
+</style>
